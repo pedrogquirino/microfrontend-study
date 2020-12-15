e0328b03c292a07bfe10e0a51b0ebf7787511930
pedrogquirino <pedrogquirino@gmail.com>
2020-12-14 22:44:52 -0300

    Adjusting

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ isSignIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            App
+            app
           </Typography>
           <Button
             color="primary"
